19e2764a76cef241248e5acd71db406fcc723113
Anmol Bhardwaj <anmolbhardwaj17@gmail.com>
2022-09-13 00:35:24 +0530

    heroku dep

--- middleware/auth.js
+++ middleware/auth.js
@@ -11,13 +11,13 @@ const verifyToken = (req, res, next) => {
     }
 
     if (!token) {
-        return res.status(403).send("A token is required for authentication");
+        return res.status(403).send({status:403, message:"A token is required for authentication"});
     }
     try {
         const decoded = jwt.verify(token, config.TOKEN_KEY);
         req.user = decoded;
     } catch (err) {
-        return res.status(401).send("Invalid Token");
+        return res.status(401).send({status:403, message:"Invalid Token"});
     }
     return next();
 };
